5dd7e6e9896af2047bfebe5f57431bba09f583de
Dhezzzzz <78156879+Dhezzzzz@users.noreply.github.com>
2025-10-30 16:58:15 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// birthday.js
+
+const msg = {
+    to: "Bagus Bimawan Sembodo",
+    message: "Happy Birthday! Wish you all the best. You are a very kind friend",
+    messages: {
+        Dhezz: "Sukses terus ya bro, semoga segala cita-cita dan impian tercapai. Thanks udah jadi teman dan mentor yang hebat.",
+        Indah: "keep shining like the moon in the dark night",
+        Zihad: "Semoga panjang umur, gampang rezekinya, jgn lupa trading",
+    },
+    from: "Your Classmates and Friends :)",
+};
+
+console.log(`Happy Birthday, ${msg.to}!`);

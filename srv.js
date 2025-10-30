cce74f2406839ac8d7d4d1043930b4e58b057507
Dhezzzzz <78156879+Dhezzzzz@users.noreply.github.com>
2025-10-30 11:51:40 +0700

    Add files via upload

---
+++ srv.js
@@ -0,0 +1,7 @@
+// .js
+const msg = "4861707079204269727468646179204261677573210a205769736820796f7520616c6c2074686520626573742e20596f752061726520612076657279206b696e6420667269656e642c20796f75206d616b65206d6520736565207468696e677320646966666572656e746c792e203a29";  
+console.log(Buffer.from(msg, 'hex').toString());  
+
+// const msg = "Happy Birthday Bagus!\n Wish you all the best. You are a very kind friend, you make me see things differently. :)";  
+// console.log(Buffer.from(msg).toString('hex'));  
+// console.log(msg);

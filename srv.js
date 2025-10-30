ae1981b264342fa7fea84b206f04a78188e24e19
Dhezzzzz <78156879+Dhezzzzz@users.noreply.github.com>
2025-10-30 16:02:53 +0700

    Add files via upload

--- srv.js
+++ srv.js
@@ -3,11 +3,20 @@ const msg = {
     to: "Bagus Bimawan Sembodo",
     message: "Happy Birthday! Wish you all the best. You are a very kind friend",
     messages: {
-        D: "Sukses terus ya bro, semoga segala cita-cita dan impian tercapai. Thanks udah jadi teman dan mentor yang hebat.",
-        I: "",
-        Z: "",
+        Dhe: "Sukses terus ya bro, semoga segala cita-cita dan impian tercapai. Thanks udah jadi teman dan mentor yang hebat.",
+        Ind: "",
+        Zih: "",
     },
     from: "Your Classmates and Friends :)",
 };
 
 console.log(`Happy Birthday, ${msg.to}!`);
+
+if (today == yourBirthday) {
+        celebrate();
+    eat(cake);
+    enjoy(life);
+}
+else {
+    keepCoding();
+}

a5d8ea7ead083f2d77c00deabada5b1b41259c10
Dhezzzzz <78156879+Dhezzzzz@users.noreply.github.com>
2025-10-30 14:57:26 +0700

    Add files via upload

--- srv.js
+++ srv.js
@@ -1,7 +1,13 @@
-// .js
-const msg = "4861707079204269727468646179204261677573210a205769736820796f7520616c6c2074686520626573742e20596f752061726520612076657279206b696e6420667269656e642c20796f75206d616b65206d6520736565207468696e677320646966666572656e746c792e203a29";  
-console.log(Buffer.from(msg, 'hex').toString());  
+// birthday.js
+const msg = {
+    to: "Bagus Bimawan Sembodo",
+    message: "Happy Birthday! Wish you all the best. You are a very kind friend",
+    messages: {
+        D: "Sukses terus ya bro, semoga segala cita-cita dan impian tercapai. Thanks udah jadi teman dan mentor yang hebat.",
+        I: "",
+        Z: "",
+    },
+    from: "Your Classmates and Friends :)",
+};
 
-// const msg = "Happy Birthday Bagus!\n Wish you all the best. You are a very kind friend, you make me see things differently. :)";  
-// console.log(Buffer.from(msg).toString('hex'));  
-// console.log(msg);
+console.log(`Happy Birthday, ${msg.to}!`);
